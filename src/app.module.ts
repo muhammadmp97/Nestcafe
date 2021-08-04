34abc9392fd32e8c8cf6df9cb4d1a08da887ea06
WebPajooh <webpajooh@gmail.com>
2021-08-04 17:37:37 +0430

    Configuration file added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,8 +7,9 @@ import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ProductsModule,
-    MongooseModule.forRoot('mongodb://localhost/nestcafe')
+    MongooseModule.forRoot(`mongodb://localhost/${process.env.DATABASE_NAME}`)
   ],
   controllers: [AppController],
   providers: [AppService],
