3d0ab9f9b6726a1b4bd688a7266f606e608f57f4
WebPajooh <webpajooh@gmail.com>
2021-08-07 08:36:58 +0430

    format a file by four spaces

--- src/products/product.schema.ts
+++ src/products/product.schema.ts
@@ -4,26 +4,26 @@ import { Document } from 'mongoose';
 export type ProductDocument = Product & Document;
 
 export interface IProduct {
-  id: string;
-  title: string;
-  description: string;
-  photo: string | null;
-  price: number;
+    id: string;
+    title: string;
+    description: string;
+    photo: string | null;
+    price: number;
 }
 
 @Schema()
 export class Product {
-  @Prop({ required: true })
-  title: string;
+    @Prop({ required: true })
+    title: string;
 
-  @Prop({ required: true })
-  description: string;
+    @Prop({ required: true })
+    description: string;
 
-  @Prop({ required: true })
-  photo: string | null;
+    @Prop({ required: true })
+    photo: string | null;
 
-  @Prop({ required: true })
-  price: number;
+    @Prop({ required: true })
+    price: number;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
