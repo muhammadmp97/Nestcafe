28ccfc61ea747108fbcfeb731d13ef04390bdd35
WebPajooh <webpajooh@gmail.com>
2021-08-14 14:39:35 +0430

    Orders count can be increased by users request

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ProductsService } from 'src/products/products.service';
 import { IOrder, Order, OrderDocument } from './orders.schema';
+const ObjectId = require('mongoose').Types.ObjectId; 
 
 @Injectable()
 export class OrdersService {
@@ -20,10 +21,11 @@ export class OrdersService {
     }
 
     async createOrder(userId: string, productId: string): Promise<IOrder> {
-        const order = await this.orderModel.create({
-            owner: userId,
-            product: productId
-        });
+        const order = await this.orderModel.findOneAndUpdate(
+            { owner: new ObjectId(userId), product: new ObjectId(productId) },
+            { $inc: { count: 1 } },
+            { upsert: true, new: true, setDefaultsOnInsert: true }
+        ).populate('product').exec();
 
         return this.prepareResponse(order); 
     }
